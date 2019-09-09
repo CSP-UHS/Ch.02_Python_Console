f30cf71b3bf3ca6ecaec7bf1d9b6beaae91ef4e8
leavertonnel <leavertonnel@urbandaleschools.com>
2019-09-09 14:10:37 -0500

    Name and Numbers

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,24 +1,21 @@
 '''
-Sign your name:________________
+Sign your name: Nellie Leaverton
 
 1.) How do you enter a single line comment in a program?
-
-
-
+you put the hashtag sign before your comment ex: #this is the comment
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs?
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
-
+b/a                 = 2.5
+b//a                = 2 #floor : cuts off everything after the decimal
+b**a                = 25
+b%a                 = 1 # modulus : oit gives you the remainder
+a+B                 = 7
+type(42)            = 42
+type(42.0)          = 42.0
+type("C3PO")        = 'C3PO'
+type(True)          = True
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -30,8 +27,8 @@ a/=2
 a+=12
 a-=7
 
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = float
 
 
 
@@ -42,16 +39,10 @@ a
 
 
 
-
-
-
 5.) Fix the code so it will calculate the average.
 x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
-
-
-
 
 
 '''
