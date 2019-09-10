c320f4284b3f3470bc13467ae5a29176e8bd65d3
alexanderrow <alexanderrow@urbandaleschools.com>
2019-09-10 14:08:36 -0500

    this is just to save the assignment

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -2,7 +2,7 @@
 Sign your name: Rowdy Alexander
 
 1.) How do you enter a single line comment in a program?
-
+using a #
 
 
 
@@ -15,9 +15,9 @@ b**a                = 25
 b%a                 = 1
 a+B                 = 7
 type(42)            = integer
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+type(42.0)          = float
+type("C3PO")        = string
+type(True)          = bool
 
 
 
@@ -30,25 +30,25 @@ a/=2
 a+=12
 a-=7
 
-a             =
-type(a)       =
+a             = 15
+type(a)       = Float
 
 
 
 4.) Fix the mistake in the following code.
-x,y = (4,5)
-a = 3(x + y)
-a
+x,y=(4,5)
+a=3*(x + y)
+a=27
 
-
+I just fixed the code so it would work
 
 
 
 
 5.) Fix the code so it will calculate the average.
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
+ave=(x+y+z)/3
+ave=4.0
 
 
 
