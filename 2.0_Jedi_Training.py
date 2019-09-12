5895b5188dfb64f0fc823e260df90b1acdff25e6
Tristan Holman <holmantri@urbandaleschools.com>
2019-09-12 13:18:56 -0500

    Finished 2

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -2,22 +2,22 @@
 Sign your name: Tristan
 
 1.) How do you enter a single line comment in a program?
-
+Add a #before the line
 
 
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs?
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                =             2.5
+b//a                =            2
+b**a                =            25
+b%a                 =            1
+a+b               =               7
+type(42)            =         class 'int'
+type(42.0)          =        class 'float'
+type("C3PO")        =     class 'str'
+type(True)          =       class 'bool'
 
 
 
@@ -30,15 +30,15 @@ a/=2
 a+=12
 a-=7
 
-a             =
-type(a)       =
+a             =      15
+type(a)       =     class 'float'
 
 
 
 4.) Fix the mistake in the following code.
 x,y = (4,5)
-a = 3(x + y)
-a
+a = 3*(x + y)
+a  = 27
 
 
 
@@ -47,8 +47,8 @@ a
 
 5.) Fix the code so it will calculate the average.
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
+ave = (x+y+z)/3
+ave = 4.0
 
 
 
