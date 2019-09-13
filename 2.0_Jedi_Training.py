5367c4a0469ad7d52b938f6f054de45e426bef20
Turtles1376 <placemat@urbandaleschools.com>
2019-09-13 13:59:48 -0500

    Finished Ch.02 Algerbra

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,23 +1,23 @@
 '''
-Sign your name:________________
+Sign your name: Matthew Place
 
 1.) How do you enter a single line comment in a program?
-
+use '#'
 
 
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs?
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 0.4
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = 7
+type(42)            = Int
+type(42.0)          = Float
+type("C3PO")        = Str
+type(True)          = Bool
 
 
 
@@ -30,15 +30,15 @@ a/=2
 a+=12
 a-=7
 
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = Float
 
 
 
 4.) Fix the mistake in the following code.
 x,y = (4,5)
 a = 3(x + y)
-a
+a 3*X+Y
 
 
 
@@ -48,7 +48,7 @@ a
 5.) Fix the code so it will calculate the average.
 x,y,z =(3,4,5)
 ave = x+y+z/3
-ave
+ave 8.6
 
 
 
