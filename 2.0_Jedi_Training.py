7f179664313e28286d2cd920d5511314b353e2fa
floryken <floryken@urbandaleschools.com>
2019-09-19 14:10:04 -0500

    a committ

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,23 +1,24 @@
 '''
-Sign your name:________________
-
+Sign your name:Kenny Flory
 1.) How do you enter a single line comment in a program?
 
-
+#this is how
 
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs?
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+NameError: name 'B' is not defined
+type(42)            = int
+type(42.0)          = float
+type("C3PO")        = str
+type(True)          = bool
 
 
 
@@ -30,15 +31,15 @@ a/=2
 a+=12
 a-=7
 
-a             =
-type(a)       =
+a             = 22.0
+type(a)       = float
 
 
 
 4.) Fix the mistake in the following code.
 x,y = (4,5)
 a = 3(x + y)
-a
+a = 27
 
 
 
@@ -47,8 +48,8 @@ a
 
 5.) Fix the code so it will calculate the average.
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
+ave = (x+y+z)/3
+ave +4.0
 
 
 
