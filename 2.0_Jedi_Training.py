ec2d27758f5ee3dc086006a46d7a54d0495650f2
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-09-11 12:05:26 -0500

    commit2

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -24,26 +24,27 @@ type(True)          = gets type (bool)
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
 
-a=2
-a*=10
-a/=2
-a+=12
-a-=7
-a             =
-type(a)       =
+a=2                     2
+a*=10                   12
+a/=2                    6
+a+=12                   18
+a-=7                    11
+a             =         11
+type(a)       =         int
 
 
 4.) What is the mistake in the following code. Fix it!
 
 x,y = (4,5)
-a = 3(x + y)
+a = 3*(x+y)
 a
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
+ave = x+y+z
+ave/=3
 ave
 
 
