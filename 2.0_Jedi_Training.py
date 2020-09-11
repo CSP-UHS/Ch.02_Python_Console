62cddb8aeec20de01ae9539c5d7713e0f87488cc
Ryan Muetzel <muetzelrya@urbandaleschools.com>
2020-09-11 10:20:46 -0500

    add console log

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -2,21 +2,22 @@
 Sign your name: Ryan Muetzel
 
 1.) How do you enter a single line comment in a program? Give an example.
-
+#by putting a hashtag before the comment
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
-
+b/a                 = 2.5  #standard division
+b//a                = 2    #division but drop the remainder
+b**a                = 25   #b^a
+b%a                 = 1     #gives the remainder of b/a
+a+B                 = 7
+type(42)            = 'integer'
+type(42.0)          = 'float'
+type("C3PO")        = 'string'
+type(True)          = 'boolean'
+b=b+1               = Adds 1 to the variable 'b' , can also use b+=1
+a,b =(2,5)          = Sets a=2 and b=5
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
@@ -26,8 +27,8 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15
+type(a)       = int
 
 
 4.) What is the mistake in the following code. Fix it!
