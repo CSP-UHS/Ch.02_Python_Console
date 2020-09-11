bf9cace7507a1456162b08ebb2d23034563da457
Ryan Muetzel <muetzelrya@urbandaleschools.com>
2020-09-11 11:02:43 -0500

    finish Jedi Training

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -11,11 +11,13 @@ b/a                 = 2.5  #standard division
 b//a                = 2    #division but drop the remainder
 b**a                = 25   #b^a
 b%a                 = 1     #gives the remainder of b/a
-a+B                 = 7
+a+B                 = 7    #literally just addition
 type(42)            = 'integer'
 type(42.0)          = 'float'
 type("C3PO")        = 'string'
 type(True)          = 'boolean'
+
+#extra notes for myself
 b=b+1               = Adds 1 to the variable 'b' , can also use b+=1
 a,b =(2,5)          = Sets a=2 and b=5
 
@@ -34,14 +36,15 @@ type(a)       = int
 4.) What is the mistake in the following code. Fix it!
 
 x,y = (4,5)
-a = 3(x + y)
+a = 3(x + y)  #the code doesnt know how to distribute to parenthesis
+a = 3*(x+y)      # should be correct
 a
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
+ave = (x+y+z)/3  #added parenthesis around the addition to make it work
 ave
 
 
