9b5a32a703392bbe81c288c07f32750e48c93040
hmarlal1 <hmarlal1@urbandaleschools.com>
2020-09-14 10:48:52 -0500

    done with everything

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -7,15 +7,15 @@ Sign your name: Lalrinpuia Hmar
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a        2.5         =2.5
+b//a        idk        =2
+b**a         idk       =25
+b%a         idk        =1
+a+B             idk    =B is not defined
+type(42)        idk    =int
+type(42.0)      idk    =float
+type("C3PO")     idk   =C3P0 is not defined
+type(True)      idk    =bool
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,22 +26,22 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             =15.0
+type(a)       =float
 
 
 4.) What is the mistake in the following code. Fix it!
 
 x,y = (4,5)
-a = 3(x + y)
-a
+a = 3(x + y) Parentheses doesn't mean multiplication. a = 3*x+3*y
+a = 3*x+3*y
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
+ave = x+y+z/3 it doesn't divide the whole thing by 3, only the z
+ave = x/3+y/3+z/3 = 4.0
 
 
 '''
