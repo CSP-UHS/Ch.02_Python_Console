f6e83b41cb5a64a75fac7ad757acc9ca04799edb
dautom21 <dautom@urbandaleschools.com>
2020-09-14 10:48:50 -0500

    Ch.02 Jedi Training completed.

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -7,15 +7,15 @@ Sign your name: Tom Dau
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 2.5=
-b//a                2=
-b**a                25=
-b%a                 1=
-a+B                 Error=
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 2.5         =2.5
+b//a                2           =2
+b**a                25          =25
+b%a                 1           =1
+a+B                 Error       =Error, "B" not identified
+type(42)            class int   =class 'int'
+type(42.0)          class float =class 'float'
+type("C3PO")        class str   =class 'str'
+type(True)          class bool  =class 'bool'
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,22 +26,22 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             15            =15.0
+type(a)       class 'int'   =class 'float'
 
 
 4.) What is the mistake in the following code. Fix it!
 
 x,y = (4,5)
-a = 3(x + y)
-a
+a = 3(x + y)    # Put a * between 3 and (x + y)
+a               # a is 27
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
+ave = x+y+z/3   # Put () around x+y+z; PEMDAS
+ave             # ave is 4.0
 
 
 '''
