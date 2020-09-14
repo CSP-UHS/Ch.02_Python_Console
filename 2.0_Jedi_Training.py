f14b156b5703f2d0bfae4e183ba3909c856c1562
thonathan <januszewskieth@urbandaleschools.com>
2020-09-14 10:55:40 -0500

    ch2 jedi training

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,5 +1,5 @@
 '''
-Sign your name:________________
+Sign your name:______ethan januszewski__________
 
 1.) How do you enter a single line comment in a program? Give an example.
 
@@ -7,18 +7,18 @@ Sign your name:________________
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = ERROR
+type(42)            = int
+type(42.0)          = float
+type("C3PO")        = str
+type(True)          = bool
 
 
-3.) What is the final output of (a) and type(a) if you enter the following 5 lines
+3.) What is the final outp ut of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
 
 a=2
@@ -26,21 +26,21 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = float
 
 
 4.) What is the mistake in the following code. Fix it!
 
-x,y = (4,5)
-a = 3(x + y)
+x,y=(4,5)
+a=3*(x+y)
 a
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
-x,y,z =(3,4,5)
-ave = x+y+z/3
+x,y,z=(3,4,5)
+ave=(x+y+z)/3
 ave
 
 
