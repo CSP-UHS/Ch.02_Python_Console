3dde3339a7f641347d7f211452760de4f54e3abb
permanjac <jacob.perman@gmail.com>
2020-09-23 09:44:59 -0500

    commit log

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -7,15 +7,15 @@ Sign your name:________________
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 1.5
+b//a                = 1
+b**a                = 9
+b%a                 = 1
+a+B                 = 5
+type(42)            = int
+type(42.0)          = float
+type("C3PO")        = str
+type(True)          = bool
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,21 +26,21 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = float
 
 
 4.) What is the mistake in the following code. Fix it!
 
 x,y = (4,5)
-a = 3(x + y)
+a = 3 * (x + y)
 a
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
+ave = (x+y+z)/3
 ave
 
 
