d745bdcbb1269b07b8a0073444fadf1425cb17b7
lopezsantiagoger <lopezsantiagoger@urbandaleschools.com>
2020-10-05 10:38:56 -0500

    committ

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -8,14 +8,14 @@ Sign your name: Gerardo Lopez
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 = divide
-b//a                = floors
-b**a                = exponet
-b%a                 = gets the remainer
-a+B                 = addition
+b/a                 = divide, 2.5
+b//a                = floors, 2
+b**a                = exponet, 25
+b%a                 = gets the remainer, 1
+a+B                 = addition, error
 # >If the output is an error record the error and try to determine what the error is!
-  > a+B
-  should be a+b unless if its different number
+  > B does not have any value
+
 type(42)            = gets type (int)
 type(42.0)          = gets type (float)
 type("C3PO")        = gets type (string)
@@ -26,12 +26,12 @@ type(True)          = gets type (bool)
 into the Python Console Window?
 
 a=2                     2
-a*=10                   12
-a/=2                    6
-a+=12                   18
-a-=7                    11
-a             =         11
-type(a)       =         int
+a*=10                   20
+a/=2                    10
+a+=12                   22
+a-=7                    15
+a             =         15.0
+type(a)       =         float
 
 
 4.) What is the mistake in the following code. Fix it!
@@ -44,8 +44,7 @@ a
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z
-ave/=3
+ave = (x+y+z)/3
 ave
 
 
