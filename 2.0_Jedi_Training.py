64f46bc48f111a06a447c6ed42793f21baf19a82
hoovermat <hoovermat@urbandaleschools.com>
2021-09-14 09:54:24 -0500

    almost done

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,5 +1,5 @@
 '''
-Sign your name:________________
+Sign your name:matthew hoover
 
 1.) How do you enter a single line comment in a program? Give an example.
 
@@ -7,15 +7,15 @@ Sign your name:________________
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
+b/a                 =2.5
+b//a                =2
+b**a                =25
+b%a                 =1
+a+B                 = there is an error because the B is a capital letter and is not defined.
+type(42)            =<class 'int'>
 type(42.0)          =
-type("C3PO")        =
-type(True)          =
+type("C3PO")        = c3po is not defined
+type(True)          =True
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,8 +26,8 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             =15.0
+type(a)       =15.0
 
 
 4.) What is the mistake in the following code. Fix it!
@@ -35,6 +35,7 @@ type(a)       =
 x,y = (4,5)
 a = 3(x + y)
 a
+the mistake is that there is no coma between the x and the y
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
@@ -42,6 +43,6 @@ a
 x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
-
+so what i did was put ave=x+y+z and then did ave/3 and that gave me the average which is 4
 
 '''
