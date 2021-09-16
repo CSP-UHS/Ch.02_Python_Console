9874f13e7853e4280377a38da248e5dc631e4901
maassjax <maassjax@urbandaleschools.com>
2021-09-16 09:46:53 -0500

    final commit

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -2,20 +2,20 @@
 Sign your name:________________
 
 1.) How do you enter a single line comment in a program? Give an example.
-
+# you add a hashtag and then your comment
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = type error
+type(42)            = integer
+type(42.0)          = float
+type("C3PO")        = string
+type(True)          = boolean
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,22 +26,22 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = float
 
 
 4.) What is the mistake in the following code. Fix it!
 
 x,y = (4,5)
-a = 3(x + y)
-a
+a = 3(x + y)  #Type error says integer object (3) is not callable
+a = 27
 
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
+ave = (x+y+z)/3
+ave 4.0
 
 
 '''
