cbdb0fd1a653771eac13da3d1c86206e48c9854c
zimmermanaid <zimmermanaid@urbandaleschools.com>
2022-09-09 08:59:26 -0500

    python practice

---
+++ Python Practice Log.py
@@ -0,0 +1,30 @@
+# This is my learning about Python
+# This is another line
+
+print(10.1//2) #Floor division
+print(6%2) #modules get remainder
+print(abs(-7)) #absolute value
+b=2
+a=7
+print(a*b)
+print(a**b)
+print(a%2)
+print()
+
+a,b=(5,4)
+print(a)
+print(b)
+
+x=8
+print(x)
+x+=1
+print(x)
+
+a="this is some text"
+b=3
+c=4.3
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
