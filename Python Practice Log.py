de2f0f0e852abd921c50e7890fb2814ccf82228f
earpowe <earpowe@urbandaleschools.com>
2022-09-09 08:59:27 -0500

    python practice

---
+++ Python Practice Log.py
@@ -0,0 +1,23 @@
+'''This is my learning about Python
+This is another line
+'''
+print(10.1//2) # floor division
+print(7%2) # remainder
+print(abs(-7)) # absolute value
+a=2
+b=7
+print(a*b)
+print(b**a)
+print(b%2)
+a,b=(5,"newo")
+print(a)
+print(b)
+x=8
+print(x)
+x=x+1
+print(x)
+a="this is sum text"
+b=3
+c=4.3
+d=True
+print(type(a))
