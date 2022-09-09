8c938e8ed9b4f670dd261e8bd2f63346c2def8d7
huynhlin <huynhlin@urbandaleschools.com>
2022-09-09 08:59:33 -0500

    python practice :)

---
+++ PythonPracticeLog.py
@@ -0,0 +1,40 @@
+#this is my learning about python
+#this is another line
+
+#hit command farwardslash to comment/uncomment things
+#two asterisks for exponent NOT carrot
+'''
+multi line comment
+makes anything between them a comment very cool
+'''
+print("Hello World")
+print(5**2)
+print(10.1//2) #floor division
+print(17%5) #modulus divides and prints remainder
+print(abs(-7))#absolute value
+a=2
+b=7
+print(a+b)
+print(b**a)
+print(b%2)
+print() #prints a blank line, could be useful for text based games ?
+
+a,b=(5,4)
+print(a)
+print(b)
+
+x=8
+print(x)
+x=x+1
+print(x)
+
+x+=1 #same as x=x+1
+
+a = "this is some text"
+b = 3
+c = 4.3
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))          
