fe915341300f342ec4880caca7fea4765c3a88c2
leglerann <leglerann@urbandaleschools.com>
2022-09-09 09:23:36 -0500

    All done :D

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,21 +1,53 @@
 '''
-Sign your name:________________
+Sign your name:Anna Legler
 
 1.) How do you enter a single line comment in a program? Give an example.
+'''
+#I can write a single line comment with a hashtag this this
 
+a=2
+b=5
+print(b/a)
+print(b//a)
+print(b**a)
+print(b%a)
+#print(a+B)Name Error
 
+print(type(42))
+print(type(42.0))
+print(type("C3PO"))
+print(type(True))
+
+a=2
+a*=10
+a/=2
+a+=12
+a-=7
+print(a)
+print(type(a))
+
+x,y = (4,5)
+a = 3*(x + y)
+print(a)
+
+x,y,z =(3,4,5)
+ave = (x+y+z)/3
+print(ave)
+
+'''
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = Name Error: name 'B' is not defined - since the b is capitalized the program doesn't
+                    know what it is because it was never defined only lowercase b is defined
+type(42)            = <class 'int'>
+type(42.0)          = <class 'float'>
+type("C3PO")        = <class 'string'>
+type(True)          = <class 'bool'>
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,22 +58,22 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = <class 'float'>
 
 
 4.) What is the mistake in the following code. Fix it!
 
 x,y = (4,5)
-a = 3(x + y)
-a
-
+a = 3*(x + y)
+print(a)
+27
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
-
+ave = (x+y+z)/3
+print(ave)
+4.0
 
 '''
---
+++ PythonPracticeLog.py
@@ -0,0 +1,43 @@
+# This is my learning about python
+# this is another line
+print(8)
+print("Hello World")
+a=2
+b=5
+print(b/a)
+print(b//a) #floor division - cuts off everything after the decimal
+print(b**a)
+print(b%a)#modulus - the remainer from when you divide something
+print(abs(-7))#absolut value
+
+a*=10
+a/=2
+a+=12
+a-=7
+print(a)
+print(type(a))
+
+
+x,y = (4,5)
+a = 3*(x + y)
+print(a)
+
+x,y,z =(3,4,5)
+ave = (x+y+z)/3
+print(ave)
+
+x=8
+x=x+1
+print(x)
+x=8
+x+=1 #short cut for x=x+1 also works for multiplication, division, and subtraction
+print(x)
+
+a = "This is some text"
+b = 3
+c = 4.3
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
