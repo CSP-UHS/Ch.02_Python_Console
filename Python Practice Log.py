193b364823280d0aa541bbabf4858531a15b4392
evittstan <evittstan@urbandaleschools.com>
2022-09-09 08:59:35 -0500

    python practice

---
+++ Python Practice Log.py
@@ -0,0 +1,38 @@
+# This is my learning about Python
+# This is another line
+# theheheh
+#
+# tieie
+# eitie
+# '''
+print(10.1//2) # floor division
+print(7%2) # modulus get the remainder
+print(abs(-7))
+a=2
+b=7
+print(a*b)
+print(b**a)
+print()
+print(b%2)
+
+a,b=(5,"Mr. Hermon")
+print(a)
+print(b)
+
+x=8
+print(x)
+x-=2  # same as x=x+1
+print(x)
+bulletsShot=0
+bulletsShot+=3
+print(bulletsShot)
+
+a = "this is some text"
+b=3
+c=4.3
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
