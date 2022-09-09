ae25b0ea413df8e231090b3d9f78f3f3eaf215d8
huynhlin <huynhlin@urbandaleschools.com>
2022-09-09 09:11:26 -0500

    jedi training

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -7,15 +7,15 @@ Sign your name:Lindy Huynh
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = name 'B' is not defined
+type(42)            = class int
+type(42.0)          = class float
+type("C3PO")        = class string
+type(True)          = class bool
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,8 +26,8 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 2
+type(a)       = integer
 
 
 4.) What is the mistake in the following code. Fix it!
@@ -36,12 +36,15 @@ x,y = (4,5)
 a = 3(x + y)
 a
 
+you cannot do 3(x + y), you need to do 3*(x+y) because python is not like traditional path
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
+
+you need to put (x+y+z) in parentheses, otherwise it divides all of them by 3 and adds the sums instead of averaging
 
 
 '''
