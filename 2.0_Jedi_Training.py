fa2723b91ef5bc0f5fb9f50fcfb5a6475f8ea2a3
haraziode <haraziode@urbandaleschools.com>
2022-09-09 09:24:22 -0500

    Completed Training 2.0 - Oded

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,47 +1,47 @@
 '''
-Sign your name:________________
+Sign your name: Oded Harazi
 
 1.) How do you enter a single line comment in a program? Give an example.
-
+#You put a hashtag/pound before the comment
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
-
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
-
+#B was undefined.
+'''
+a=2
+b=5
+print(b/a)
+print(b//a)
+print(b**a)
+print(b%a)
+#print(a+B) #B is undefined
+print(type(42))
+print(type(42.0))
+print(type("C3PO"))
+print(type(True))
+'''
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
-
+'''
 a=2
 a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
-
+print(a) #15 - Correct prediction
+print(type(a)) #Integer - Incorrect prediction
+'''
 
 4.) What is the mistake in the following code. Fix it!
-
-x,y = (4,5)
-a = 3(x + y)
-a
-
+'''
+x,y=4,5 #Shouldn't have parenthesis
+a=3*(x+y) #No multiplication sign
+print(a) #forgot the "print()"
+'''
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
-
-x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
-
-
 '''
+x,y,z=3,4,5 #shouldn't have parenthesis around the variables
+ave=(x+y+z)/3 #missing parenthesis
+print(ave) #No "print()"
---
+++ Python Practice Log - Oded.py
@@ -0,0 +1,44 @@
+#This is my learning about Python
+#This is how you comment without affecting the other code
+
+#Command\ comments out an entire section of code so it doesn't run
+#'''You can also put three small apostrophe to change an area out of comment to code'''
+
+
+print("Hello World")
+print(10.1//2) #floor division, cuts off everything behind the decimal
+print(7%2) #modulus get the remainder
+print(abs(-7)) #absolute value
+a=2
+b=7
+print(a*b)
+print(b**a)
+print() #prints blank line
+print(b%2)
+
+a,b=(5,"Mr.Hermon") #assigns multiple varibles at the same time
+print(a)
+print(b)
+
+x=8
+print(x)
+x+=1 #reassigning the variable (whatever x is plus one) #same as x=x+1
+print(x)
+x*=2 #same as x*2
+print(x)
+bulletShot=0
+bulletShot+=3
+print(bulletShot)
+
+a="text"
+b=3
+c=4.3
+d=True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+a,b=5,"Mr.Hermon"
+print(a,b)
