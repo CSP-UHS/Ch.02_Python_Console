b12ae80b9e716495a0de8df33bee4e44788c8c8e
fletchermat <fletchermat@urbandaleschools.com>
2022-09-09 08:59:38 -0500

    python practice

---
+++ Python Practice Log.py
@@ -0,0 +1,40 @@
+'''# This is my learning about python
+# This is another line  command ?/ is comment/uncomment'''
+print(5**3) # exponent
+print(10.1//2) # floor division cuts off every thing beyond decimal
+print(7%2) # modulus finds remainder
+print(abs(-7)) # absolute value
+a=2 # variable
+b=7
+print(a) # variable
+print("a") # not variable is string=str print(3) integer=int
+print() # blank line
+print(b*a)
+print(b**a)
+print(b%2)
+print()
+
+a,b=(5,"Matthew Fletcher")
+print(a)
+print(b)
+
+x=8
+print(x)
+x=x+1 # shortcut x+=1
+print(x)
+x*=2 #same as x=x*2
+print(x)
+bulletsShot=0
+bulletsShot=bulletsShot+3 # easeir to bulletsShot+=3
+print(bulletsShot)
+
+a="this is some text"
+b=3
+c=4.3
+d= True
+print(type(a)) # string
+print(type(b)) # integer
+print(type(c)) # float
+print(type(d)) # boolean
+
+print()
