426afe0365cd220f6fdce925b4d3aee67296ee1b
sandoval-ordagicben <sandoval-ordagicben@urbandaleschools.com>
2022-09-09 08:59:19 -0500

    finsihed

---
+++ PythonPracticeLog.py
@@ -0,0 +1,35 @@
+# Comment of the day chicken butt.
+# Another line
+'''
+jxcjckxzjkcxzj
+cxznjncxcxxc
+cxzknkncxzncxz
+cxzknncxzncxz
+'''
+print(5//3) #floor Division
+print(10%2) #Modules get the remainder
+print(abs(-7)) #Absolute vaule
+a=2
+b=7
+print(a+b)
+print(b**a)
+print(b%2)
+print(a)
+print("a")
+
+x=8
+print(x)
+x*=2 #same as x=x*2
+print(x)
+bulletShot=0
+bulletShot+=3
+print(bulletShot)
+
+a= "Text yes"
+b=3
+c=4.3
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
