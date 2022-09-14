45c3148e8acd108ef58cf8467ba4a4e687b98cbc
fletchermat <fletchermat@urbandaleschools.com>
2022-09-14 08:54:28 -0500

    python practice

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,46 +1,53 @@
 '''
-Sign your name:________________
+Sign your name:Matthew Fletcher
 
 1.) How do you enter a single line comment in a program? Give an example.
-
+# this is a single line comment
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
+'''
+a=2
+b=5
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+print(b/a)
+print(b//a)
+print(b**a)
+print(b%a)
+# print(a+B) 'B' is not defined
+print(type(42))
+print(type(42.0))
+print(type("C3PO"))
+print(type(True))
 
-
+'''
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
-
+'''
+print()
 a=2
 a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+#15 float
+print(a)
+print(type(a))
 
+'''
 
 4.) What is the mistake in the following code. Fix it!
-
+'''
 x,y = (4,5)
-a = 3(x + y)
-a
+# a = 3(x + y)
+a=3*(x + y)  #needs multiplacation symbol
+print(a)
 
-
+'''
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
+ave = x+y+z/3   ave = (x+y+z)/3   needs parentheses becaused of PEMDAS
 ave
 
 
