b770fa569372d0df096cc89a57e783fb3dfe31e8
donohoand <donohoand@urbandaleschools.com>
2022-09-19 10:09:53 -0500

    2.0 Jedi_training

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,47 +1,48 @@
-'''
-Sign your name:________________
+#Sign your name:Andrew Donoho
 
-1.) How do you enter a single line comment in a program? Give an example.
+#1.) How do you enter a single line comment in a program? Give an example.
 
-
-2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
-What are the outputs? If the output is an error record the error and try to determine what the error is!
-
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+#whats up gamers
 
 
-3.) What is the final output of (a) and type(a) if you enter the following 5 lines
-into the Python Console Window?
 
-a=2
-a*=10
-a/=2
-a+=12
-a-=7
-a             =
-type(a)       =
-
-
-4.) What is the mistake in the following code. Fix it!
-
-x,y = (4,5)
-a = 3(x + y)
-a
+#2.) Enter a=2 and b=5 in the Python Console window and then all of the following.
+#What are the outputs? If the output is an error record the error and try to determine what the error is!
+a=2 b=5
+b//a =
+#b**a                =
+#b%a                 =
+#a+B                 =
+#type(42)            =
+#type(42.0)          =
+#type("C3PO")        =
+#type(True)          =
 
 
-5.) What is the mistake in the following code so it will calculate the average. Fix it!
+#3.) What is the final output of (a) and type(a) if you enter the following 5 lines
+#into the Python Console Window?
 
-x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
+#a=2
+#a*=10
+#a/=2
+#a+=12
+#a-=7
+#a             =
+#type(a)       =
 
 
-'''
+#4.) What is the mistake in the following code. Fix it!
+
+#x,y = (4,5)
+#a = 3(x + y)
+#a
+
+
+#5.) What is the mistake in the following code so it will calculate the average. Fix it!
+
+#x,y,z =(3,4,5)
+#ave = x+y+z/3
+#ave
+
+
+
