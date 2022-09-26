d364a4a042f08bcdfae672ea736de2d2f21e9053
donohoand <donohoand@urbandaleschools.com>
2022-09-25 23:10:30 -0500

    2.0_jedi_training_done_hopefully

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -8,15 +8,18 @@
 
 #2.) Enter a=2 and b=5 in the Python Console window and then all of the following.
 #What are the outputs? If the output is an error record the error and try to determine what the error is!
-a=2 b=5
-#b//a                = invalid syntax
-#b**a                =
-#b%a                 =
-#a+B                 =
-#type(42)            =
-#type(42.0)          =
-#type("C3PO")        =
-#type(True)          =
+#a=2
+#b=5
+#c= (42)
+#print(c)
+#b//a                = 2
+#b**a                = 25
+#b%a                 =1
+#a+B                 = B is not defined,need to put lower case b
+#type(42)            = 42
+#type(42.0)          = 42.0
+#type("C3PO")        = C3PO is not defined need to defined what it is
+#type(True)          = true
 
 
 #3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -27,22 +30,25 @@ a=2 b=5
 #a/=2
 #a+=12
 #a-=7
-#a             =
-#type(a)       =
+#print(type(a))
+#a             =15.0
+#type(a)       =float
 
 
 #4.) What is the mistake in the following code. Fix it!
 
-#x,y = (4,5)
-#a = 3(x + y)
-#a
+#x=4
+#y=5
+
+#a = 3*(x+y)
+#print(a)
 
 
 #5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
-#x,y,z =(3,4,5)
-#ave = x+y+z/3
-#ave
+x,y,z =(3,4,5)
+ave = (x+y+z)/3
+print(ave)
 
 
 
