996e08e54d1c0d979a50fc1f3eaa7a054600562a
leglerann <leglerann@urbandaleschools.com>
2022-10-04 22:17:05 -0500

    Final change

--- PythonPracticeLog.py
+++ PythonPracticeLog.py
@@ -1,49 +1,7 @@
 # This is my learning about python
 # this is another line
-'''
-print(8)
-print("Hello World")
-a=2
-b=5
-print(b/a)
-print(b//a) #floor division - cuts off everything after the decimal
-print(b**a)
-print(b%a)#modulus - the remainer from when you divide something
-print(abs(-7))#absolut value
+import random
 
-a*=10
-a/=2
-a+=12
-a-=7
-print(a)
-print(type(a))
-
-
-x,y = (4,5)
-a = 3*(x + y)
-print(a)
-
-x,y,z =(3,4,5)
-ave = (x+y+z)/3
-print(ave)
-
-x=8
-x=x+1
-print(x)
-x=8
-x+=1 #short cut for x=x+1 also works for multiplication, division, and subtraction
-print(x)
-
-a = "This is some text"
-b = 3
-c = 4.3
-d = True
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-'''
-#
 # a = 4
 # b = 5
 # c = 20
@@ -62,6 +20,57 @@ print(type(d))
 
 # for i in range(0,101,2):
 #     print(i)
+#
+# for i in range(100, -1, -2):
+#     print(i)
+#
+# total = 0
+# for i in range(5):
+#     num = int(input("Enter a number: "))
+#     total += num
+# print("Your total is ", total)
 
-for i in range(100, -1, -2):
-    print(i)
+# total = 0
+# for i in range(10):
+#     total += 1
+#     for j in range(10):
+#         total += 1
+# print(total)
+
+#While not done:
+# done = False
+# perserverance = 0
+# while not done:
+#     quit = input("Do you want to quit? (type y)")
+#     if quit.lower() == "y":
+#         done = True
+#     else:
+#         perserverance += 1
+# print("Goodbye! Your perserverance level is ", perserverance)
+
+# for i in range(10):
+#     num = random.randrange(50)
+#     print(num)
+
+num = random.randrange(101)
+guess = int(input("Guess my number: "))
+wrongGuess = 0
+playAgain = "y"
+while playAgain == "y":
+    while guess != num:
+        if guess > num:
+            wrongGuess += 1
+            print("No that's too high")
+            guess = int(input("Guess again!"))
+        elif guess < num:
+            wrongGuess += 1
+            print("No that's too low")
+            guess = int(input("Guess again!"))
+    print("Good job you got it right")
+    print("That took you", wrongGuess, "guesses")
+    playAgain = input("Would you like to play again? (type y) ")
+    if playAgain == "y":
+        wrongGuess = 0
+        num = random.randrange(101)
+        guess = int(input("Guess my number: "))
+print("Goodbye!")
