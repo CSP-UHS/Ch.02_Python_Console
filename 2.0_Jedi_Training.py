81a247bf2da619a7d71c712950f6616354bd8b1d
Marc Hermon <hermonm@urbandaleschools.com>
2024-09-17 12:09:14 -0500

    Update 2.0_Jedi_Training.py
    removed PDF log upload

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,9 +1,8 @@
 '''
 2.0 Jedi Training (20pts)  Name:________________
 
-UPLOAD YOUR CH 2 PDF LOG! (6pts)
 
-1.) How do you enter a single line comment in a program? Give an example. (1pt)
+1.) How do you enter a single line comment in a program? Give an example. (2pt)
 
 
 2.) If a=2 and b=5, predict all of the following below and record your predictions. (9pts)
@@ -22,7 +21,7 @@ type("C3PO")        =
 type(True)          =
 
 
-3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (2pts)
+3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (3pts)
 into the Python Console Window? Then actually do it and record the output and comment on differences
 between your predictions and the output.
 
@@ -35,14 +34,14 @@ a             =
 type(a)       =
 
 
-4.) What is the mistake in the following code? Comment it and fix it!  (1pt)
+4.) What is the mistake in the following code? Comment it and fix it!  (3pt)
 
 x,y = (4,5)
 a = 3(x + y)
 a
 
 
-5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (1pt)
+5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (3pt)
 
 x,y,z =(3,4,5)
 ave = x+y+z/3
